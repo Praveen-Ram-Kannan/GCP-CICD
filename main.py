6632ab156e1bea6026eb7aa246de3ecf3ba7ffc7
Praveen-Ram-Kannan <73990324+Praveen-Ram-Kannan@users.noreply.github.com>
2021-10-25 14:51:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from google.cloud import bigquery
+
+bq_client = bigquery.Client()
+
+def ingest(data, context):
+  
+  bucket_name = data['bucket']
+  file_name = data['name']
+  table = "dataanalysis-330007.EmployeeDetails.Employee"
+
+  job_config = bigquery.LoadJobConfig(
+      schema=[
+          bigquery.SchemaField("ID", "INTEGER"),
+          bigquery.SchemaField("Prefix", "STRING"),
+          bigquery.SchemaField("FirstName", "STRING"),
+          bigquery.SchemaField("MiddleName", "STRING"),
+          bigquery.SchemaField("LastName", "STRING"),
+          bigquery.SchemaField("Gender", "STRING"),
+          bigquery.SchemaField("EMail", "STRING"),
+      ],
+      skip_leading_rows=1,
+      source_format=bigquery.SourceFormat.CSV,
+  )
+  uri = f"gs://{bucket_name}/{file_name}"
+
+  load_job = bq_client.load_table_from_uri(
+      uri, table, job_config=job_config
+  )
+
+  load_job.result()  # Waits for the job to complete.
+
+  destination_table = bq_client.get_table(table)  # Make an API request.
+  print("Loaded {} rows.".format(destination_table.num_rows))
